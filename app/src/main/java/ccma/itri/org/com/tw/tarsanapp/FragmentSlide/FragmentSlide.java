ac4a88fc173281852a2fb51f585eefc4de1bac9b
A40503 <itriA40503@itri.org.tw>
2016-10-20 16:21:14 +0800

    log layoutresId

--- app/src/main/java/ccma/itri/org/com/tw/tarsanapp/FragmentSlide/FragmentSlide.java
+++ app/src/main/java/ccma/itri/org/com/tw/tarsanapp/FragmentSlide/FragmentSlide.java
@@ -3,6 +3,7 @@ package ccma.itri.org.com.tw.tarsanapp.FragmentSlide;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,6 +33,7 @@ public class FragmentSlide extends Fragment {
 
         if(getArguments() != null && getArguments().containsKey(ARG_LAYOUT_RES_ID)){
             layoutResId = getArguments().getInt(ARG_LAYOUT_RES_ID);
+            Log.d("layoutResId", String.valueOf(layoutResId));
         }
     }
 
